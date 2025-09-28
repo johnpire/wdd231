8771ca80dd89eb4f58d35c1e53f3c1689afa37d5
johnpire <ijcwasbornonthe15thofseptember@gmail.com>
2025-09-28 14:38:06 +0800

    enhanced events section

---
+++ chamber/scripts/events.js
@@ -0,0 +1,19 @@
+const images = document.querySelectorAll('.slider img');
+    const prevBtn = document.querySelector('.prev');
+    const nextBtn = document.querySelector('.next');
+    let index = 0;
+
+    function showImage(i) {
+      images.forEach(img => img.classList.remove('active'));
+      images[i].classList.add('active');
+    }
+
+    prevBtn.addEventListener('click', () => {
+      index = (index - 1 + images.length) % images.length;
+      showImage(index);
+    });
+
+    nextBtn.addEventListener('click', () => {
+      index = (index + 1) % images.length;
+      showImage(index);
+    });
